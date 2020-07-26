04fb9e0a82ecc7eb96190d16cbf0cda04b17f39f
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-07-26 02:36:20 -0700

    change component view to card

--- src/ components/MenuComponent.js
+++ src/ components/MenuComponent.js
@@ -1,75 +1,27 @@
 import React, { Component } from 'react';
-import {Media} from 'reactstrap'
+import {Card, CardBody,CardTitle, CardImgOverlay, CardText, CardImg } from 'reactstrap'
 
 
 class Menu extends Component {
-    constructor(props){
-        super (props);
-        this.state = {
-            dishes:[
-                {
-                    id: 0,
-                    name:'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-                 {
-                    id: 1,
-                    name:'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label:'',
-                    price:'1.99',
-                    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        
-                },
-                {
-                    id: 2,
-                    name:'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label:'New',
-                    price:'1.99',
-                    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-                 {
-                    id: 3,
-                    name:'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label:'',
-                    price:'2.99',
-                    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        
-                }
-
-            ],
-        }
-    }
-
+  // here in the constructor we are only using props so no need for a physical constructor 
     render() {
-        const menu=this.state.dishes.map(i => {
-            return <div key={i.id} className='col-12 mt-5'>
-                <Media tag='li'>
-                    <Media left middle>
-                        <Media object src={i.image} alt={i.name}></Media>
-                    </Media>
-                    <Media body className='ml-5'>
-                        <Media heading>{i.name}</Media>
-                        <p>{i.description}</p>
-                    </Media>
-                </Media>
+        const menu=this.props.dishes.map(i => {
+            return <div key={i.id} className='col-12 col-md-5 mt-5'>
+                <Card>
+                    <CardImg width="100%" src={i.image} alt={i.name}></CardImg>
+                    <CardImgOverlay>
+                        <CardTitle>{i.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
             </div>
         });
         return(
             <div className='container'>
                 <div className='row'>
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
                 </div>
             </div>
-        )
-        
+        )   
     }
 }
 
--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
 import Menu from './ components/MenuComponent'
 import { Navbar, NavbarBrand } from 'reactstrap';
+import {DISHES} from './shared/dishes'
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state = { 
+      dishes: DISHES
+     }
+    }
+    render() {
+      return (
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu  dishes={this.state.dishes}/>
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+      );
+  }
 }
-
 export default App;
